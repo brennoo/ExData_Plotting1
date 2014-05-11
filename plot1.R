e128ee69bdcd44a530000d2c5aedec6a2a8c768b
brennoo <brenno@bsd.com.br>
2014-05-11 15:04:16 -0300

    Create plot1.R
    initial commit with final solution

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# Plot #1
+
+# Read data
+data <- read.table("./R/household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors =FALSE)
+
+# Specify the data range
+data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# Open file
+png("plot1.png", width = 480, height = 480)
+
+# Plot data
+hist(as.numeric(data$Global_active_power), col="red", main = "Global Active Power",xlab="Global Active Power (kilowatts)")
+
+# Close file
+dev.off()
